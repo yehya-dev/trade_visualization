30be1c6f2d8b210fb78ec44e5f3e54513400c423
Yehya Usman <yehya.dev@gmail.com>
2021-06-07 10:48:42 +0530

    change readme and added an image to show the ui

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Trade Visualiation
 
-View Multiple Trade View Charts And Signals On A Single Chrome Tab 
+*View Multiple Trade View Charts And Signals On A Single Chrome Tab*
+
+(prototype)
+### Description
+- loads mulitple asset/quote pairs from a json file and displays the trading data of a browser tab
+- Helpful for analysing multiple assets at once
+- The chart and technical analysis of every pair is displayed side by side (widgets from [Tradingview](https://www.tradingview.com/))
+- Every page can have a maximum of 25 pairs by default (can be changed)
+- Arrow keys can be used to switch between pages
+___
+### View
+![image_not_found](view.png)
+___
+
+### How To Run
+navigate to the project direcotory and run<br>
+
+`uvicorn graphs:app`
+___
+### dependencies
+- starlette==0.14.1
+- uvicorn==0.13.3
--- graphs.py
+++ graphs.py
@@ -1,9 +1,11 @@
 from starlette.applications import Starlette
 from starlette.routing import Route, Mount
 from starlette.templating import Jinja2Templates
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, HTMLResponse
 from starlette.staticfiles import StaticFiles
 import json
+
+TEMP_LIST = True
 
 templates = Jinja2Templates('templates')
 
@@ -55,13 +57,25 @@ async def pump_coins(request):
         })
 
 async def fav_coins(request):
-        needed_coins = get_needed_coins(file='fav_asset.json')
+        needed_coins = get_needed_coins(file= 'pump_test.json' if not TEMP_LIST else 'temp_list.json')
         needed_coins = divide_into_lists(25, needed_coins)
         return JSONResponse({
             'coins': needed_coins
         })
 
+
+async def homepage(request):
+    return HTMLResponse(
+'''
+<a href="/assets">/assets</a><br>
+<a href="/assets?inter=1440/1D">/assets?inter=1440/1D</a><br>
+<a href="/assets?type=pump">/assets?type=pump</a><br>
+<a href="/assets?type=pump&inter=1440/1D">/assets?type=pump&inter=1440/1D</a>
+''')
+
+
 routes = [
+    Route('/', homepage),
     Route('/assets', assets),
     Route('/pump_coins', pump_coins),
     Route('/fav_coins', fav_coins),
